41fc392b93308723994fd3a25c5beb49ccaae889
supersergiy <sergiy.popovich@gmail.com>
2018-04-29 13:42:46 -0400

    img read

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from scipy.io import loadmat, savemat
+from scipy.ndimage import imread
 from flask import request
 
 app = Flask(__name__)
@@ -10,11 +11,17 @@ def process():
    frames_path = request.args.get('frames_path')
    frames = loadmat(frames_path)['frames']
    rect_path = request.args.get('rect_path')
-   rect = loadmat(rect_path)['rect']
+   init_rect = loadmat(rect_path)['rect'][0]
+
    out_rects = []
    for f in frames:
-       out_rects.append(rect[0])
-   #import pdb; pdb.set_trace()
+       image_path = f[0][0]
+       image_data = imread(image_path)
+       ## Result bounding box computation:
+       # TODO
+       result_bbox = init_rect
+       out_rects.append(result_bbox)
+
    savemat(output_rect_path, {"out_rects": out_rects})
    return output_rect_path
 
